8fbd6ee4cdfb85976019ce7e921974e168fae95c
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2022-02-12 21:39:36 +0800

    2 player on screen

--- Assets/Scripts/CreateAndJoinRooms.cs
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
@@ -11,13 +12,17 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 
     public void CreateRoom()
     {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
         PhotonNetwork.CreateRoom(createInput.text);
+        Debug.Log("Create Room: " + createInput.text);
     }
 
     // Note that when you create room, you automatically join that room.
     public void JoinRoom()
     {
         PhotonNetwork.JoinRoom(joinInput.text);
+        Debug.Log("Joined to Room: " + joinInput.text);
     }
 
     // When you to load a multiplayer scene, you must use PhotonNetwork.LoadLevel and not the SceneManager.LoadScene
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
+    PhotonView view;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * 2, Input.GetAxis("Vertical") * 2);
+        if (view.IsMine)
+        {
+            body.velocity = new Vector2(Input.GetAxis("Horizontal") * 2, Input.GetAxis("Vertical") * 2);
+        }
     }
 }
