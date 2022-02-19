166009de34e8b7d4cd7036e0caa9c5dd40e7666e
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2022-02-19 23:25:54 +0800

    enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -19,6 +19,9 @@ public class Enemy : MonoBehaviour
         float distance1 = Vector2.Distance(transform.position, players[0].transform.position);
         float distance2 = Vector2.Distance(transform.position, players[1].transform.position);
 
+        Debug.Log(distance1);
+        Debug.Log(distance2);
+
         if (distance1 < distance2)
         {
             nearestPlayer = players[0];
